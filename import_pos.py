a3df0c41d9774d0f06a7900ac58bb08805661287
ewheeler <evanmwheeler@gmail.com>
2009-12-29 20:13:56 -0500

    Initial commit

---
+++ import_pos.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+import sys
+import os
+
+import codecs
+import csv
+import datetime
+from decimal import Decimal as D
+
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.core.management import setup_environ
+
+try:
+    import settings
+    setup_environ(settings)
+except:
+    sys.exit("No settings found")
+
+
+from wall.models import *
+
+def import_csv(args):
+
+    # use codecs.open() instead of open() so all characters are utf-8 encoded
+    # BEFORE we start dealing with them (just in case)
+    # rU option is for universal-newline mode which takes care of \n or \r etc
+    csvee = codecs.open("purchaseOrders.csv", "rU", encoding='utf-8', errors='ignore')
+
+    # sniffer attempts to guess the file's dialect e.g., excel, etc
+    dialect = csv.Sniffer().sniff(csvee.read(1024))
+    # for some reason, sniffer was finding '"'
+    dialect.quotechar = '"'
+    csvee.seek(0)
+    # DictReader uses first row of csv as key for data in corresponding column
+    reader = csv.DictReader(csvee, dialect=dialect, delimiter=",", quoting=csv.QUOTE_ALL, doublequote=True)
+
+    try:
+        print 'begin rows'
+        po_count = 0
+        for row in reader:
+            def has_datum(row, key):
+                if row.has_key(key):
+                    if row[key] != "":
+                        return True
+                return False
+
+            def has_data(row, key_list):
+                if False in [has_datum(row, key) for key in key_list]:
+                    return False
+                else:
+                    return True
+            if has_datum(row, 'PO_OOI_REGION'):
+                region, created = Region.objects.get_or_create(name=row['PO_OOI_REGION'])
+
+                if has_datum(row, 'PO_OOI_COUNTRY'):
+                    country, created = Country.objects.get_or_create(name=row['PO_OOI_COUNTRY'])
+                    if created:
+                        country.region = region
+                        country.save()
+                    if has_datum(row, 'CATEGORY_DESC'):
+                        try:
+                            category, created = Category.objects.get_or_create(name=row['CATEGORY_DESC'])
+                        except Exception, e:
+                            print 'BANG category:'
+                            print e
+
+                        if has_data(row, ['SUPPLIER_COUNTRY', 'SUPPLIER_NAME']):
+                            try:
+                                supplier_country, created = Country.objects.get_or_create(name=row['SUPPLIER_COUNTRY'])
+                                supplier, created  = Supplier.objects.get_or_create(name=row['SUPPLIER_NAME'], country=supplier_country)
+                            except Exception, e:
+                                print 'BANG supplier:'
+                                print e
+
+                            if has_data(row, ['MATERIAL_TYPE', 'MATERIAL']):
+                                try:
+                                    material, created = Material.objects.get_or_create(name=row['MATERIAL'], material_type=row['MATERIAL_TYPE'])
+                                except Exception, e:
+                                    print 'BANG material:'
+                                    print e
+                                try:
+    # [1] "ProMS.uid"                  "PO_TYPE"                   
+    # [3] "MATERIAL_TYPE"              "Item.Line.Value"           
+    # [5] "Old.Book.G"                 "New.Book.G"                
+    # [7] "CATEGORY_DESC"              "MATERIAL"                  
+    # [9] "MATERIAL_ADDITIONAL_INFO"   "PO_REFERENCE"              
+    #[11] "PO_ISSUE_DATE"              "PO_DELIVERY_DATE"          
+    #[13] "TAD"                        "TAFD"                      
+    #[15] "BUDGET_YEAR"                "SUPPLIER_COUNTRY"          
+    #[17] "SUPPLIER_NAME"              "QUANTITY_ORDERED"          
+    #[19] "UNIT_PRICE..US..."          "PO_AMOUNT_USD"             
+    #[21] "PO.Total.Value"             "Fraction.of.Total.PO.Value"
+    #[23] "PO_OOI_COUNTRY"             "PO_OOI_REGION"             
+    #[25] "X"                          "X.1"                       
+    #[27] "Columns.introduced.by.us"   "Columns.from.ProMS"        
+    #[29] "X.2" 
+                                    try:
+                                        new_purchase = Purchase(material=material,\
+                                            unit_price=D(row['UNIT_PRICE (US $)']),\
+                                            quantity=int(row['QUANTITY_ORDERED']),\
+                                            supplier=supplier)
+                                        new_purchase.save()
+                                    except Exception, e:
+                                        print 'BANG purchase:'
+                                        print e
+
+                                    def format_date(datestr):
+                                        # expecting MM/DD/YY
+                                        if datestr is not None:
+                                            if datestr != '':
+                                                month, day, year = datestr.split('/')
+                                                millenium = '20'
+                                                real_year = millenium + year
+                                                return datetime.date(month=int(month),\
+                                                    day=int(day), year=int(real_year))
+                                            else:
+                                                return None
+                                        else:
+                                            return None
+
+                                    try:
+                                        clean_issue_date = format_date(row['PO_ISSUE_DATE'])
+                                        clean_delivery_date = format_date(row['PO_DELIVERY_DATE'])
+                                        clean_tad = format_date(row['TAD'])
+                                        clean_tafd = format_date(row['TAFD'])
+                                    except Exception, e:
+                                        print 'BANG dates:'
+                                        print e
+                                        print row
+
+                                    # expecting -DD.DD%
+                                    # chop off % and cast as decimal
+                                    try:
+                                        clean_fraction = D(row['Fraction of Total PO Value'][:-1])
+                                    except Exception, e:
+                                        print 'BANG fraction:'
+                                        print e
+
+                                    try:
+                                        new_po = PurchaseOrder(country=country,\
+                                            issue_date=clean_issue_date,\
+                                            delivery_date=clean_delivery_date,\
+                                            tad=clean_tad, tafd=clean_tafd,\
+                                            reference=row['PO_REFERENCE'],\
+                                            type=row['PO_TYPE'],\
+                                            total_value=D(row['PO Total Value']),\
+                                            fraction_of_tv=D(clean_fraction),\
+                                            amount_usd=D(row['PO_AMOUNT_USD']),\
+                                            budget_year=int(row['BUDGET_YEAR']),\
+                                            item_line_value=row['Item Line Value'][2],\
+                                            old_book_g=row['Old Book G'][2],\
+                                            new_book_g=row['New Book G'][2],\
+                                            category=category,\
+                                            proms_uid=row['ProMS uid'],\
+                                            additional_info=row['MATERIAL_ADDITIONAL_INFO'],\
+                                            purchase=new_purchase)
+                                        new_po.save()
+                                        po_count += 1
+                                        if po_count % 1000 == 0:
+                                            print(po_count)
+                                    except Exception, e:
+                                        print 'BANG po:'
+                                        print e
+
+                                except Exception, e:
+                                    print(e)
+                                    continue
+                else:
+                    print 'NO COUNTRY'
+                    continue
+            continue
+
+
+    except csv.Error, e:
+        # TODO handle this error?
+        print('%d : %s' % (reader.reader.line_num, e))
+
+
+if __name__ == "__main__":
+    import_csv(sys.argv) 
---
+++ wall/models.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+
+from django.db import models
+
+class Country(models.Model):
+    name = models.CharField(max_length=160, blank=True, null=True)
+    region = models.ForeignKey('Region', blank=True, null=True)
+
+class Region(models.Model):
+    name = models.CharField(max_length=160, blank=True, null=True)
+    
+class PurchaseOrder(models.Model):
+    PO_TYPE_CHOICES = (
+        ('X', 'X'),
+        ('E', 'E'),
+    )
+    ITEM_LINE_VALUE_CHOICES = (
+        ('A', '< 100'),
+        ('B', '> 100'),
+    )
+    OLD_BOOK_G_CHOICES = (
+        ('A', '< 500'),
+        ('B', '500 - 1000'),
+        ('C', '1000 - 2500'),
+        ('D', '> 2500'),
+    )
+    NEW_BOOK_G_CHOICES = (
+        ('A', '< 1000'),
+        ('B', '1000 - 2500'),
+        ('C', '> 2500'),
+    )
+    country = models.ForeignKey(Country)
+    issue_date = models.DateField(blank=True, null=True)
+    delivery_date = models.DateField(blank=True, null=True)
+    tad = models.DateField(blank=True, null=True)
+    tafd = models.DateField(blank=True, null=True)
+    reference = models.CharField(max_length=160, blank=True, null=True)
+    type = models.CharField(max_length=1, choices=PO_TYPE_CHOICES, blank=True, null=True)
+    total_value = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    fraction_of_tv = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    amount_usd = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    budget_year = models.PositiveIntegerField(blank=True, null=True)
+    item_line_value = models.CharField(max_length=1, choices=ITEM_LINE_VALUE_CHOICES, blank=True, null=True)
+    old_book_g = models.CharField(max_length=1, choices=OLD_BOOK_G_CHOICES, blank=True, null=True)
+    new_book_g = models.CharField(max_length=1, choices=NEW_BOOK_G_CHOICES, blank=True, null=True)
+    category = models.ForeignKey('Category', blank=True, null=True)
+    proms_uid = models.PositiveIntegerField(blank=True, null=True)
+    additional_info = models.TextField(blank=True, null=True)
+    purchase = models.ForeignKey('Purchase')
+
+class Purchase(models.Model):
+    material = models.ForeignKey('Material')
+    unit_price = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    quantity = models.PositiveIntegerField(blank=True, null=True)
+    supplier = models.ForeignKey('Supplier', blank=True, null=True)
+
+class Material(models.Model):
+    # 243 of these
+    MATERIAL_TYPE_CHOICES = (
+        ('CO', 'CO'),
+        ('NW', 'NW'),
+        ('UN', 'UN'),
+    )
+    name = models.CharField(max_length=160, blank=True, null=True)
+    material_type = models.CharField(max_length=2, choices=MATERIAL_TYPE_CHOICES, blank=True, null=True)
+
+class Category(models.Model):
+    # 26 of these
+    name = models.CharField(max_length=160, blank=True, null=True)
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=160, blank=True, null=True)
+    country = models.ForeignKey(Country)
