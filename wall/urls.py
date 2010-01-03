6cc2fdbdbde5e773e1a391dffc3a958b42abbac2
ewheeler <evanmwheeler@gmail.com>
2010-01-03 02:10:15 -0500

    serve static files

--- wall/urls.py
+++ wall/urls.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
+import os
+
 from django.conf.urls.defaults import *
 import views
 
 urlpatterns = patterns('',
+    # serve assets via django, during development
+    url(r'^assets/js/(?P<path>.*)$', "django.views.static.serve",
+    {"document_root": os.path.dirname(__file__) + "/static/javascripts"}),
     url(r'^$', views.index),
 )
