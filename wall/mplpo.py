09e4dff6e42ffdc3588f9fd93ebc7f3f8c671f18
ewheeler <evanmwheeler@gmail.com>
2010-02-24 18:03:18 -0500

    legend and colors

--- wall/mplpo.py
+++ wall/mplpo.py
@@ -6,7 +6,7 @@ def get_all_amts():
     print 'getting PO amounts...'
     all_amount_usd = PurchaseOrder.objects.values_list('amount_usd', flat=True).order_by('amount_usd')
     print 'found %s, converting to floats...' % len(all_amount_usd)
-    all_amount_usd_f = [float(x) for x in all_amount_usd]
+    all_amount_usd_f = [float(x) for x in all_amount_usd if x > 0.0]
     print 'converted %s amounts!' % len(all_amount_usd_f)
     all = all_amount_usd_f[0:362549]
     return all
@@ -15,25 +15,30 @@ def get_regional_amts():
     all = []
     regs = Region.objects.all()
     for reg in regs:
-        print 'getting PO amounts for %s' % reg.name
-        ramts = PurchaseOrder.objects.values_list('amount_usd', flat=True).filter(country__region=reg).order_by('amount_usd')
-        ramts_f = [float(x) for x in ramts]
-        ramts_f.append(reg.name)
-        all.append(ramts_f)
+        if reg.name is not None:
+            if reg.name not in ['2006']:
+                print 'getting PO amounts for %s' % reg.name
+                ramts = PurchaseOrder.objects.values_list('amount_usd',\
+                    flat=True).filter(country__region=reg).order_by('amount_usd')
+                ramts_f = [float(x) for x in ramts if x > 0.0]
+                ramts_f.append(reg.name)
+                all.append(ramts_f)
     return all
 
 def make_hist(amts, max=20000, step=200, htype=None, line=False):
 
     steps = range(0,max,step)
+    pretty = ['red','green','blue','orange','yellow','springgreen',\
+                'slategray','fuchsia']
+
     if htype == 'barstacked':
         print 'barstacked!'
         for amt_list in amts:
-            if amt_list[-1] is not None:
-                if amt_list[-1] != '2006':
-                    n, bins, patches = plt.hist(amt_list[:-1], steps, normed=0,\
-                        alpha=0.75, histtype='barstacked', label=amt_list[-1])
-
+            n, bins, patches = plt.hist(amt_list[:-1], steps, normed=0,\
+                histtype='barstacked', label=amt_list[-1],\
+                facecolor=pretty.pop())
     else:
+        print 'plain'
         n, bins, patches = plt.hist(amts, steps, normed=0, alpha=0.85, histtype='bar')
 
     def best_fit():
