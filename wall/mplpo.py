5fa4c3d7b1f165444fb5cb7b40caf985db70d79d
ewheeler <evanmwheeler@gmail.com>
2010-02-25 12:03:48 -0500

    added country and category hists, improved colors

--- wall/mplpo.py
+++ wall/mplpo.py
@@ -25,18 +25,44 @@ def get_regional_amts():
                 all.append(ramts_f)
     return all
 
-def make_hist(amts, max=20000, step=200, htype=None):
+def get_country_amts(reg):
+    all = []
+    region = Region.objects.get(name__iexact=reg)
+    countries = Country.objects.filter(region=region)
+    for country in countries:
+        print 'getting PO amounts for %s' % country.name
+        camts = PurchaseOrder.objects.values_list('amount_usd',\
+                flat=True).filter(country=country).order_by('amount_usd')
+        camts_f = [float(x) for x in camts if x > 0.0]
+        camts_f.append(country.name)
+        all.append(camts_f)
+    return all
+
+def get_category_amts(country_name):
+    all = []
+    country = Country.objects.get(name__iexact=country_name)
+    cats = Category.objects.all()
+    for cat in cats:
+        print 'geting %s PO amounts for %s' % (country.name, cat.name)
+        camts = PurchaseOrder.objects.values_list('amount_usd',\
+                flat=True).filter(country=country).filter(category=cat)\
+                .order_by('amount_usd')
+        camts_f = [float(x) for x in camts if x > 0.0]
+        camts_f.append(cat.name)
+        all.append(camts_f)
+    return all
+
+def make_hist(amts, max=20000, step=200, htype=None, height=40000, legend=True):
 
     steps = range(0,max,step)
-    pretty = ['red','green','blue','orange','yellow','springgreen',\
-                'slategray','fuchsia']
+    cmap = plt.get_cmap('Paired', len(amts))
 
     if htype == 'barstacked':
         print 'barstacked!'
-        for amt_list in amts:
+        for index, amt_list in enumerate(amts):
             n, bins, patches = plt.hist(amt_list[:-1], steps, normed=0,\
                 histtype='barstacked', label=amt_list[-1],\
-                facecolor=pretty.pop())
+                facecolor=cmap(index))
     else:
         print 'plain'
         n, bins, patches = plt.hist(amts, steps, normed=0, alpha=0.85, histtype='bar')
@@ -44,6 +70,7 @@ def make_hist(amts, max=20000, step=200, htype=None):
     plt.grid(True)
     plt.xlabel('PO Amount USD')
     plt.ylabel('Number of POs')
-    plt.axis([0,max,0,40000])
-    plt.legend()
+    plt.axis([0,max,0,height])
+    if legend:
+        plt.legend()
     plt.show()
